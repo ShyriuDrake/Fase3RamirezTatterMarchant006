793e5a018d1dfeacdf1866fa904c799eb336bc1f
ivan marchant lillo <iv.marchant@alumnos.duoc.cl>
2020-12-03 22:11:29 -0300

    arreglo de footer, boton de inicio de sesion y definicion de usuarios

--- nenuko/nenuko/urls.py
+++ nenuko/nenuko/urls.py
@@ -31,13 +31,13 @@ urlpatterns = [
     
 ]
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+#router = routers.DefaultRouter()
+#router.register(r'users', views.UserViewSet)
+#router.register(r'groups', views.GroupViewSet)
 
-urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+#urlpatterns = [
+#    path('', include(router.urls)),
+#    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+#]
 
 urlpatterns+= static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
